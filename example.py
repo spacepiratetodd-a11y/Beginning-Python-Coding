4ce634aed944b11d7f058dec02f906ca0a481662
spacepiratetodd-a11y <spacepiratetodd@gmail.com>
2025-10-13 13:36:31 -0600

    Working on f-strings

--- example.py
+++ example.py
@@ -1,2 +1,12 @@
-print("Hello World")
-input("What is your name?")
+#Create input prompts to have the user input the values for the variables.
+name = input("What is your name? ")
+hobby = input("What is your hobby? ")
+yearOfBirth = input("What year were you born? ")
+currentYear = input("What is the current year? ")
+
+#Creat f-strings to display the input values for the variables.
+print(f"Hello, {name}!")
+print(f"I also enjoy {hobby}.")
+
+#Calculate the user's age using Python acceptable operations and display it.
+print(f"You are {int(currentYear) - int(yearOfBirth)} years old.")
