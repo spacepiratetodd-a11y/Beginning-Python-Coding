67769c6d0687675100db8e01a695424bd602ca22
spacepiratetodd-a11y <spacepiratetodd@gmail.com>
2025-10-22 13:21:20 -0600

    shut down python interpreter
    push

--- loop.py
+++ loop.py
@@ -3,7 +3,11 @@ print(f"Hello, {name}!")
 
 answer = input("Would you like to continue? ('y' for yes or 'n' for no)")
 
+loopCount = 1
+
 while answer == 'y' or answer == 'Y':
     print("You chose to continue!")
-    answer = input("Would you like to continue? ('y' for yes or 'n' for no)")
+    print(f"You have continued {loopCount} times.")
+    answer = input("Would you like to continue? ('y' for yes or 'n' for no)")  
+    loopCount += 1
 print("You chose to stop. Goodbye!")    
